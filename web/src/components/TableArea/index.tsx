b0f8bb60cfd9609b70be6848efce67e14401d1e4
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-12-29 18:45:42 -0300

    add customizacao componentes tablearea e tableitem

--- web/src/components/TableArea/index.tsx
+++ web/src/components/TableArea/index.tsx
@@ -3,11 +3,13 @@ import { TableItem } from '../TableItem';
 
 type Props<T extends Record<string, any>> = {
   list: T[];
+  columnNames?: Partial<Record<keyof T, { label: string; width?: number }>>; // Updated type
+  columnFormatters?: Partial<Record<keyof T, (value: T[keyof T]) => React.ReactNode>>;
 };
 
-export function TableArea<T extends Record<string, any>>({ list }: Props<T>) {
+export function TableArea<T extends Record<string, any>>({ list, columnNames = {}, columnFormatters = {} }: Props<T>) {
   if (!list.length) {
-    return null; 
+    return null;
   }
 
   const columns = Object.keys(list[0]);
@@ -17,13 +19,15 @@ export function TableArea<T extends Record<string, any>>({ list }: Props<T>) {
       <thead>
         <tr>
           {columns.map((column, index) => (
-            <C.TableHeadColumn key={index}>{column}</C.TableHeadColumn>
+            <C.TableHeadColumn key={index} style={{ width: columnNames[column]?.width }}>
+              {columnNames[column]?.label || column}
+            </C.TableHeadColumn>
           ))}
         </tr>
       </thead>
       <tbody>
         {list.map((item, index) => (
-          <TableItem key={index} item={item} />
+          <TableItem key={index} item={item} columnFormatters={columnFormatters} />
         ))}
       </tbody>
     </C.Table>
--- web/src/components/TableItem/index.tsx
+++ web/src/components/TableItem/index.tsx
@@ -2,15 +2,20 @@ import * as C from './styles';
 
 type Props<T extends Record<string, any>> = {
   item: T;
+  columnFormatters?: Partial<Record<keyof T, (value: T[keyof T]) => React.ReactNode>>;
 };
 
-export function TableItem<T extends Record<string, any>>({ item }: Props<T>) {
+export function TableItem<T extends Record<string, any>>({ item, columnFormatters = {} }: Props<T>) {
   const columns = Object.keys(item);
 
   return (
     <C.TableLine>
       {columns.map((column, index) => (
-        <C.TableColumn key={index}>{item[column]}</C.TableColumn>
+        <C.TableColumn key={index} >
+          {columnFormatters[column as keyof T]
+            ? columnFormatters[column as keyof T]!(item[column])
+            : item[column]}
+        </C.TableColumn>
       ))}
     </C.TableLine>
   );
