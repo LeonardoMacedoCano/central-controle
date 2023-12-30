c87c8aecf8b1c75e4408889bc3c27ea9da73fe47
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-12-29 21:30:03 -0300

    add styles para lista despesa

---
+++ web/src/pages/ListaDespesas/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+    text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+    color: #000;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
+`;
