b1161b29f390473ef2d962491044f81a3129d85d
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-12-31 16:36:13 -0300

    corrigir data das despesas e add nova variavel de ambiente

--- server/src/main/java/com/backend/centraldecontrole/dto/DespesaRequestDTO.java
+++ server/src/main/java/com/backend/centraldecontrole/dto/DespesaRequestDTO.java
@@ -1,6 +1,16 @@
 package com.backend.centraldecontrole.dto;
 
+import com.backend.centraldecontrole.util.CustomDateDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import java.util.Date;
 
-public record DespesaRequestDTO(Long idCategoria, String descricao, Double valor, Date data) {
+public record DespesaRequestDTO(
+        Long idCategoria,
+        String descricao,
+        Double valor,
+        @JsonDeserialize(using = CustomDateDeserializer.class)
+        Date data
+) {
+
 }
--- server/src/main/java/com/backend/centraldecontrole/util/DateUtil.java
+++ server/src/main/java/com/backend/centraldecontrole/util/DateUtil.java
@@ -1,14 +1,28 @@
 package com.backend.centraldecontrole.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.Date;
 import java.util.TimeZone;
 
 public class DateUtil {
+    private static String timeZoneId;
+
+    @Value("${spring.jackson.time-zone}")
+    public void setTimeZoneId(String timeZoneId) {
+        DateUtil.timeZoneId = timeZoneId;
+    }
 
     public static Date getDataAtual() {
-        TimeZone timeZone = TimeZone.getTimeZone("America/Sao_Paulo");
+        TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
+
         Date currentDate = new Date();
         currentDate.setTime(currentDate.getTime() + timeZone.getRawOffset());
+
         return currentDate;
+    }
+
+    public static TimeZone getTimeZone() {
+        return TimeZone.getTimeZone(timeZoneId);
     }
 }
