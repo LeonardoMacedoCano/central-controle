d0d9c2e45564171e53ca85a2fd73d1fc7b06e3f6
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2023-12-30 23:11:13 -0300

    add config despesas

---
+++ web/src/config/Despesas/DespesaColumnFormatters.ts
@@ -0,0 +1,14 @@
+type ColumnFormattersArgs = {
+  categoriaMap: Record<number, string>;
+};
+
+export const DespesaColumnFormatters = ({ categoriaMap }: ColumnFormattersArgs) => ({
+  id: (value: string | number | Date) => Number(value),
+  descricao: (value: string | number | Date) => String(value),
+  valor: (value: string | number | Date) => Number(value).toFixed(2),
+  data: (value: string | number | Date) => new Date(value).toLocaleDateString(),
+  idCategoria: (value: string | number | Date) => {
+    const categoryId = typeof value === 'number' ? value : Number(value);
+    return categoriaMap[categoryId] || 'Categoria Desconhecida';
+  },
+});
---
+++ web/src/config/Despesas/DespesaInputFields.ts
@@ -0,0 +1,6 @@
+export const DespesaInputFields = [
+    { label: 'Categoria', type: 'select' as const, key: 'categoria' as const },
+    { label: 'Descricao', type: 'text' as const, key: 'descricao' as const },
+    { label: 'Valor', type: 'number' as const, key: 'valor' as const },
+    { label: 'Data', type: 'date' as const, key: 'data' as const },
+  ];
