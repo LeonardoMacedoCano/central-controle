27eb06dab062773ccb1031cf3176fd2bb1b813ae
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-07 19:43:23 -0300

    alinhar codigo

--- server/src/main/java/com/backend/centraldecontrole/dto/DespesaRequestDTO.java
+++ server/src/main/java/com/backend/centraldecontrole/dto/DespesaRequestDTO.java
@@ -3,7 +3,6 @@ package com.backend.centraldecontrole.dto;
 import com.backend.centraldecontrole.util.CustomDateDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-
 import java.util.Date;
 
 public record DespesaRequestDTO(
--- server/src/main/java/com/backend/centraldecontrole/dto/DespesaResponseDTO.java
+++ server/src/main/java/com/backend/centraldecontrole/dto/DespesaResponseDTO.java
@@ -1,7 +1,14 @@
 package com.backend.centraldecontrole.dto;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
-public record DespesaResponseDTO(Long id, Long idCategoria, String descricao, Double valor, @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime data) {
+public record DespesaResponseDTO(
+        Long id,
+        Long idCategoria,
+        String descricao,
+        Double valor,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime data
+) {
 }
