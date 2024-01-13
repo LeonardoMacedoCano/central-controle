38923b3e60425c1751457f79e0ebb9116cccb448
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-13 01:13:44 -0300

    atualizar config despesa

--- web/src/config/Despesas/DespesaColunasFormat.ts
+++ web/src/config/Despesas/DespesaColunasFormat.ts
@@ -2,13 +2,40 @@ type DespesaColunasFormatArgs = {
   categoriaMap: Record<number, string>;
 };
 
-export const DespesaColunasFormat = ({ categoriaMap }: DespesaColunasFormatArgs) => ({
-  id: (value: string | number | Date) => Number(value),
-  descricao: (value: string | number | Date) => String(value),
-  valor: (value: string | number | Date) => Number(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
-  data: (value: string | number | Date) => new Date(value).toLocaleDateString(),
-  idCategoria: (value: string | number | Date) => {
-    const categoryId = typeof value === 'number' ? value : Number(value);
-    return categoriaMap[categoryId] || 'Categoria Desconhecida';
-  },
-});
+type DespesaColunasFormatReturnType = {
+  id: (value: string | number | boolean | Date) => React.ReactNode;
+  descricao: (value: string | number | boolean | Date) => React.ReactNode;
+  valor: (value: string | number | boolean | Date) => React.ReactNode;
+  data: (value: string | number | boolean | Date) => React.ReactNode;
+  idCategoria: (value: string | number | boolean | Date) => React.ReactNode;
+};
+
+export const DespesaColunasFormat = ({ categoriaMap }: DespesaColunasFormatArgs): DespesaColunasFormatReturnType => {
+  const isDate = (value: any): value is Date => value instanceof Date;
+
+  return {
+    id: (value: string | number | boolean | Date): React.ReactNode => Number(value),
+    descricao: (value: string | number | boolean | Date): React.ReactNode => String(value),
+    valor: (value: string | number | boolean | Date): React.ReactNode => {
+      if (typeof value === 'number') {
+        return Number(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+      }
+      return null;
+    },
+    data: (value: string | number | boolean | Date): React.ReactNode => {
+      if (typeof value === 'string') {
+        return new Date(value).toLocaleDateString();
+      } else if (value instanceof Date) {
+        return value.toLocaleDateString();
+      }
+      return null;
+    },
+    idCategoria: (value: string | number | boolean | Date): React.ReactNode => {
+      if (isDate(value)) {
+        return categoriaMap[value.valueOf()] || 'Categoria Desconhecida';
+      }
+      const categoryId = typeof value === 'number' ? value : Number(value);
+      return categoriaMap[categoryId] || 'Categoria Desconhecida';
+    },
+  };
+};
