e167b09c82cbcecadd3dccb6e03c9c73178764b6
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-14 14:29:53 -0300

    Atualizar scripts db

--- server/scripts/scripts_db.sql
+++ server/scripts/scripts_db.sql
@@ -7,16 +7,34 @@ CREATE TABLE usuario (
 
 CREATE TABLE categoriadespesa (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    descricao VARCHAR(255)
+    descricao VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE despesa (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    idusuario INT,
-    idcategoria INT,
-    descricao VARCHAR(255),
-    valor FLOAT,
-    data DATETIME,
+    idusuario INT NOT NULL,
+    idcategoria INT NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    valor FLOAT NOT NULL,
+    data DATETIME NOT NULL,
     FOREIGN KEY (idusuario) REFERENCES usuario(id),
     FOREIGN KEY (idcategoria) REFERENCES categoriadespesa(id)
+);
+
+CREATE TABLE categoriatarefa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tarefa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idusuario INT NOT NULL,
+    idcategoria INT NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    datainclusao DATETIME NOT NULL,
+    dataprazo DATETIME NOT NULL,
+    finalizado CHAR(1) DEFAULT 'N',
+    FOREIGN KEY (idusuario) REFERENCES usuario(id),
+    FOREIGN KEY (idcategoria) REFERENCES categoriatarefa(id)
 );
