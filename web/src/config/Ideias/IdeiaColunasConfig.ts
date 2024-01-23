f76eb595d4e83e22c79a3f88347de51109ed1202
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-01-22 22:48:04 -0300

    add config Ideia

---
+++ web/src/config/Ideias/IdeiaColunasConfig.ts
@@ -0,0 +1,8 @@
+export const IdeiaColunasConfig = {
+    id: { label: 'ID', width: 50 },
+    idCategoria: { label: 'Categoria', width: 150 },
+    titulo: { label: 'Titulo' },
+    descricao: { label: 'Descrição' },
+    dataPrazo: { label: 'Dt Prazo', width: 100 },
+    finalizado: { label: 'Finalizado', width: 50 },
+  };
