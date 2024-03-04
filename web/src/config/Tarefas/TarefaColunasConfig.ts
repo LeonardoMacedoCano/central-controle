07b9411fe8d6421999dd8dd2c061efb80169393d
LeonardoMacedoCano <leonardomacedocano@gmail.com>
2024-03-03 23:41:18 -0300

    excluir config

--- web/src/config/Tarefas/TarefaColunasConfig.ts
+++
@@ -1,8 +0,0 @@
-export const TarefaColunasConfig = {
-    id: { label: 'ID', width: 50 },
-    idCategoria: { label: 'Categoria', width: 150 },
-    titulo: { label: 'Titulo' },
-    descricao: { label: 'Descrição' },
-    dataPrazo: { label: 'Dt Prazo', width: 100 },
-    finalizado: { label: 'Finalizado', width: 50 },
-  };
